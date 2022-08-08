52b91d40e046fb1de0c4f087fc59b9fb5af4863d
Vladyslav Denysiuk <vl.denysiuk@gmail.com>
2022-08-08 13:04:11 +0300

    Heroku commut

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2022_07_28_162718) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
@@ -57,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_162718) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.boolean "preferable"
+    t.boolean "preferable", default: false
     t.index ["slug"], name: "index_calculators_on_slug", unique: true
     t.index ["uuid"], name: "index_calculators_on_uuid", unique: true
   end
@@ -142,7 +141,6 @@ ActiveRecord::Schema.define(version: 2022_07_28_162718) do
     t.string "provider"
     t.string "uid"
     t.boolean "blocked", default: false
-    t.boolean "receive_recomendations", default: false
     t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
